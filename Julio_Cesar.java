8e18c35cff94ca484ebbfc8e617314ec34b732ed
Jaesbyp <96183427+Jaesbyp@users.noreply.github.com>
2022-02-18 12:30:40 -0500

    Not Idea

---
+++ Julio_Cesar.java
@@ -0,0 +1,28 @@
+import javax.swing.JOptionPane;
+import java.util.ArrayList;
+public class Julio_Cesar {
+    public static void main(String[] args) {
+        //Encriptor
+        String frase = JOptionPane.showInputDialog("Ingresa una frase");
+        int [] numero = new int[frase.length()];
+        for(int i = 0; i < frase.length() ;i++) 
+        {
+            int ascii = (int) frase.charAt(i);
+            numero[i] = (ascii);
+        }
+
+        for(int i = 0; i< numero.length ; i++)
+            {
+            numero[i] += 5;
+            }
+
+            for(int x: numero)
+            {
+                char y = (char) x;
+                System.out.print(y);
+            }
+             
+        }
+    }   
+    
+
